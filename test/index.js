2720716a40bad0cd97aa6f98b64fa70850d8777b
Kevin Locke <kevin@kevinlocke.name>
2021-02-17 15:03:53 -0700

    Test exported interface
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test/index.js
+++ test/index.js
@@ -1,16 +1,58 @@
 /**
- * @copyright Copyright 2016 Kevin Locke <kevin@kevinlocke.name>
+ * @copyright Copyright 2021 Kevin Locke <kevin@kevinlocke.name>
  * @license MIT
  */
 
 'use strict';
 
+const assert = require('assert');
+
 const assertShim = require('..');
 
-describe('assertShim', () => {
-  it('does something', (done) => {
-    // Assert something about assertShim here
-    assertShim.func();
-    done();
+const nodeVersion = process.version.slice(1).split('.').map(Number);
+
+function describeAssert(assertTest) {
+  it('is a function and the same as its .ok property', () => {
+    assert.strictEqual(assertTest, assertTest.ok);
+    assert.strictEqual(typeof assertTest, 'function');
   });
-});
+
+  it('.strict is a function', () => {
+    assert.strictEqual(typeof assertTest.strict, 'function');
+  });
+
+  it('.strict behaves like .ok', () => {
+    assertTest.strict(true);
+  });
+
+  it('.strict === .strict.strict', () => {
+    assert.strictEqual(assertTest.strict, assertTest.strict.strict);
+  });
+
+  it('.strict.ok === .ok', () => {
+    assert.strictEqual(assertTest.strict.ok, assertTest.ok);
+  });
+
+  it('has .doesNotMatch function', () => {
+    assertTest.doesNotMatch('Hello World', /^Goodbye/);
+  });
+
+  it('has .match function', () => {
+    assertTest.match('Hello World', /^hello/i);
+  });
+
+  it('.strict.doesNotMatch === .doesNotMatch', () => {
+    assert.strictEqual(assertTest.strict.doesNotMatch, assertTest.doesNotMatch);
+  });
+
+  it('.strict.match === .match', () => {
+    assert.strictEqual(assertTest.strict.match, assertTest.match);
+  });
+}
+
+// Run tests against the assert module on Node.js >= 15 to ensure correctness.
+if (nodeVersion[0] >= 15) {
+  describe('assert', () => describeAssert(assert));
+}
+
+describe('assertShim', () => describeAssert(assertShim));
